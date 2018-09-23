85200f5fd1ae019c7f3fb7f8dba2f005a5ef9ca9
dmujaddid <robyeka@email.unikom.ac.id>
2018-09-23 11:19:25 +0700

    first commit

---
+++ src/pbo6k/pkg10116599/latihan9/teks/PBO6K10116599Latihan9Teks.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6k.pkg10116599.latihan9.teks;
+
+/**
+ *
+ * @author
+ * NAMA     : Robby Eka Purnama
+ * KELAS    : PBO6K
+ * NIM      : 10116599
+ * Deskripsi Program : Program penggunaan tipe data Teks.
+ * 
+ */
+public class PBO6K10116599Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
